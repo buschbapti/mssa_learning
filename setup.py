6c0b1123ce242314b7719dc52c5bf5c3f92861d5
Baptiste Busch <baptiste.busch@gmail.com>
2018-04-09 10:52:09 +0200

    move dependencies to the requirements.txt

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
     license="GNU General Public License 3",
     description="MSSA_Learning",
     long_description=open('README.md').read(),
-    install_requires= ["pandas", "numpy", "fastdtw", "progressbar"],
     package_dir={'': "src"},
     packages=["mssa_learning", "mssa_learning.tools", "mssa_learning.models"]
 )
